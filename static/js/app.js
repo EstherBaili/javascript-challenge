a283a04cff2be3af4a78531c800f5b6b31c57743
Baili Li <li116tao@gmail.com>
2020-08-24 22:02:25 +0930

    starter1

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+//loop through data and console.log each data object
+data.forEach(function(data) {
+    console.log(data);
+});
+
+//append one table row 'tr' for each object
+data.forEach(function(data){
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(function([key, value]){
+    console.log(key, value);
+    //use d3 to append 1 cell per object
+    var cell = row.append("td");
+    //update each cell
+    cell.text(value);
+
+    });
+});
+
+
